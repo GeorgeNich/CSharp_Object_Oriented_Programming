d1487c54bc4eb725b89213b4f9d811660e9b39e5
George <80371942+GeorgeNich@users.noreply.github.com>
2024-02-10 05:15:23 +1100

    hello world simple introduction

---
+++ OOP_SideProjects/Task_1.1P_Hello_World/Program1_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Name1_1
+{
+    class Program1_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is Your name?");
+            string name = Console.ReadLine();
+
+            if (name == "George") {
+                Console.WriteLine("Welcome back " + name);
+            } else if (name == "Dom") {
+
+                Console.WriteLine("Hello Hello " + name + " How are you feeling?");
+
+            } else 
+            {
+                Console.WriteLine("Hello, I don't believe we've met stranger.");
+            }
+
+            
+        }
+    }
+}
