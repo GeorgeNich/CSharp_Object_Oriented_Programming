6c0057bcbb7030f5b6a3bc38da59904003f9b74f
George <80371942+GeorgeNich@users.noreply.github.com>
2024-02-13 22:31:09 +1100

    Add files via upload

---
+++ OOP_SideProjects/Task_3.1P_NameTester_ControlFlowStatements/Guessname.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+
+        public static string thename, getnamestring;
+        public static bool forever = true; // not used
+
+        public enum Menuoption
+        {
+            TestName,
+
+            GuessThatName,
+
+            Quit,
+        };
+
+        static void Main()
+             {
+                try{
+                
+                Menuoption userSelection;
+                 do
+                 {
+
+                     userSelection = ReadUserOption();
+
+                    switch (userSelection)
+                    {
+                        case Menuoption.TestName:
+                            Console.WriteLine("What is the name that you wanted?");
+                            GetName();
+                             break;
+
+                         case Menuoption.GuessThatName:
+                             GuessName();
+                             break;
+                         case Menuoption.Quit:
+                             Console.WriteLine("You've decided to quit.");
+                             Console.WriteLine("the name was " + thename);
+                             break;   
+                     }
+
+                 }
+                 while (userSelection != Menuoption.Quit);
+             } catch (FormatException)
+             {
+                 Console.WriteLine("Use only numbers for the selection phase");
+             }
+             }
+
+        
+        //functions
+    public static void GetName()
+    {
+        thename = Console.ReadLine();
+        foreach (char x in thename)             
+        {
+            if (char.IsDigit(x))
+            {
+                Console.Write("Numbers are not allowed please only enter letters\n");  
+            }
+            break;
+        }
+    
+    }
+
+
+        public static void GuessName()
+        {
+            do{
+            Console.WriteLine("What is the name youre going to GUESS");
+            getnamestring = Console.ReadLine();
+            if (getnamestring == thename)
+            {
+                Console.WriteLine("well done you got it correct!");
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+
+        } while (getnamestring != thename);
+        }
+
+
+        //read user option input then enum
+        public static Menuoption ReadUserOption()
+        {
+            Console.WriteLine("1: Input Name");
+            Console.WriteLine("2: Guess Name");
+            Console.WriteLine("3: Giveup(Quit)");
+            Console.WriteLine("Choose an option: 1, 2 or 3");
+
+
+            int number = 0;
+            number = Convert.ToInt32(Console.ReadLine());
+            return (Menuoption)(number - 1);
+
+
+        }
+    }
+}
+
---
+++ OOP_SideProjects/Task_3.1P_NameTester_ControlFlowStatements/Guessnumber.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+
+
+        public static int target, guess;
+        public static bool forever = true; // not used
+
+        public enum Menuoption
+        {
+            TestNumber,
+
+            GuessThatNumber,
+
+            Quit,
+        };
+
+        static void Main()
+        {
+            Menuoption userSelection;
+            target = new Random().Next(100) + 1;
+            try
+            {
+                do
+                {
+
+                    userSelection = ReadUserOption();
+
+                    switch (userSelection)
+                    {
+                        case Menuoption.TestNumber:
+                            Console.WriteLine("Number generated, press 2 to guess");
+                            GetNumber();
+                            break;
+                        case Menuoption.GuessThatNumber:
+                            GuessNumber();
+                            break;
+                        case Menuoption.Quit:
+                            Console.WriteLine("You've decided to quit.");
+                            Console.WriteLine("the number was " + target);
+                            break;
+                    }
+
+                }
+                while (userSelection != Menuoption.Quit);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("***Please only type integers e.g 11 or 57***");
+            }
+            finally
+            {
+                Console.WriteLine("code run");
+            }
+        }
+        
+//get number
+        public static void GetNumber()
+        {
+            target = new Random().Next(100);
+
+        }
+
+        
+            //guess number
+        public static void GuessNumber()
+        {
+            do
+            {
+                Console.WriteLine("What is the number youre going to GUESS");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == target)
+                {
+                    Console.WriteLine("well done you got it correct!");
+                }
+                else if (guess > target)
+                {
+                    Console.WriteLine("Try again, guess is too high");
+                }
+                else if (guess < target)
+                {
+                    Console.WriteLine("Guess it too low. try again");
+                }
+            } while (guess != target);
+        }
+        //read user option input then enum
+        public static Menuoption ReadUserOption()
+        {
+            Console.WriteLine("1: Test Number");
+            Console.WriteLine("2: Guess Number");
+            Console.WriteLine("3: Giveup(Quit)");
+            Console.WriteLine("Choose an option: 1, 2 or 3");
+
+
+            int number = 0;
+            number = Convert.ToInt32(Console.ReadLine());
+            return (Menuoption)(number - 1);
+
+
+        }
+    }
+}
+
