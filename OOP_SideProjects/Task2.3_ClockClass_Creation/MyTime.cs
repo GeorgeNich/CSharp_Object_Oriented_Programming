c0da4059f70ce36af395f3569a4a3757361e0889
George <80371942+GeorgeNich@users.noreply.github.com>
2024-02-13 22:28:16 +1100

    making a clock

---
+++ OOP_SideProjects/Task2.3_ClockClass_Creation/MyTime.cs
@@ -0,0 +1,201 @@
+﻿using System;
+namespace Clock
+{
+    class MyTime
+    {
+
+
+        private int hour = 0;
+        private int minute = 0;
+        private int second = 0;
+
+        public MyTime()
+        {
+
+        }
+
+
+        public MyTime(int hour, int minute, int second)
+        {
+
+            SetTime(hour, minute, second);
+            
+
+        }
+
+        public void SetTime(int hour, int minute, int second)
+        {
+            
+            SetHour(hour);
+            SetMinute(minute);
+            SetSecond(second);
+
+        }
+
+        public void SetHour(int hour)
+        {
+            if (hour < 24 && hour >= 0)
+            {
+                this.hour = hour;
+            }
+            else
+            {
+                Console.WriteLine("Only values between 0 and 24 are valid as an hour input!!");
+            }
+        }
+
+        public void SetMinute(int minute)
+        {
+            if (minute < 60 && minute >= 0)
+            {
+                this.minute = minute;
+            }
+            else
+            {
+                Console.WriteLine("Only values between 0 and 60 are valid as minute input");
+            }
+  
+        }
+
+        public void SetSecond(int second)
+        {
+            if (second < 60 && second >= 0)
+            {
+                this.second = second;
+            }
+            else
+            {
+                Console.WriteLine("Only values between 0 and 60 are valid as a second input");
+            }
+        }
+
+        public int GetHour()
+        {
+            return hour;
+        }
+
+        public int GetMinute()
+        {
+            return minute;
+        }
+
+        public int GetSecond()
+        {
+            return second;
+        }
+
+
+        new public string ToString()
+        {
+
+            return hour.ToString("00") + ":" + minute.ToString("00") + ":" + second.ToString("00");
+
+        }
+
+        public MyTime NextSecond()
+        {
+            second++;
+            if (second > 59)
+            {
+                second = 0;
+                NextMinute();
+                return this;
+            }
+            else
+            {
+                return this;
+            }
+
+        }
+
+        public MyTime NextMinute()
+        {
+            minute++;
+            if (minute > 59)
+            {
+
+                minute = 0;
+                NextHour();
+                return this;
+
+            }
+            else
+            {
+                return this;
+            }
+        }
+
+        public MyTime NextHour()
+        {
+            hour++;
+            if (hour >  23)
+            {
+
+                hour = 0;
+                return this;
+
+            }
+            else
+            {
+                return this;
+            }
+        }
+
+        public MyTime PreviousSecond()
+        {
+            second--;
+
+            if (second < 0)
+            {
+                second = 59;
+                PreviousMinute();
+                return this;
+            }
+            else
+            {
+                return this;
+            }
+
+        }
+
+        public MyTime PreviousMinute()
+        {
+            minute--;
+
+            if (minute < 0)
+            {
+                minute = 59;
+                PreviousHour();
+                return this;
+            }
+            else
+            {
+                return this;
+            }
+
+        }
+
+        public MyTime PreviousHour()
+        {
+            hour--;
+
+            if (hour < 0)
+            {
+                hour = 23;
+                return this;
+            }
+            else
+            {
+                return this;
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ OOP_SideProjects/Task2.3_ClockClass_Creation/TestMyTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Clock
+{
+    class TestMyTime
+    {
+        static void Main(string[] args)
+        {
+            MyTime GeorgesTime = new MyTime(00, 00, 00);
+            //Current time
+            Console.WriteLine(GeorgesTime.ToString());
+
+            //future time
+            Console.WriteLine(GeorgesTime.NextSecond().ToString());
+            Console.WriteLine(GeorgesTime.NextMinute().ToString());
+            Console.WriteLine(GeorgesTime.NextHour().ToString());
+            Console.WriteLine(GeorgesTime.NextHour().ToString()); 
+
+            //Past Time
+            Console.WriteLine(GeorgesTime.PreviousHour().ToString());
+            Console.WriteLine(GeorgesTime.PreviousMinute().ToString());
+            Console.WriteLine(GeorgesTime.PreviousSecond().ToString());
+
+            // set time
+            GeorgesTime.SetHour(2);
+            GeorgesTime.SetMinute(33);
+            GeorgesTime.SetSecond(77);
+            Console.WriteLine(GeorgesTime.ToString());
+            GeorgesTime.SetTime(41, 72, 97);
+            Console.WriteLine(GeorgesTime.ToString());
+
+            // Get functions
+
+            Console.WriteLine(GeorgesTime.GetHour());
+            Console.WriteLine(GeorgesTime.GetMinute());
+            Console.WriteLine(GeorgesTime.GetSecond());
+
+
+
+
+
+
+
+
+        }
+    }
+}
